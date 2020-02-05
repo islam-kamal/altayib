3ec533d74ef3acc9e805d1e04c7267d53deff428
hanaa <ahkamkhalaf@gmail.com>
2020-02-05 10:10:20 +0200

    frist step in altayib app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:altayib/UI/Home.dart';
+void main(){
+  runApp(
+    new MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+
+    ),
+  );
+}
